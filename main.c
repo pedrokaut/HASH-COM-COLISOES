dcd3aec7b514d066f38a5cd52b657ce342e8bde5
pedrokaut <lexus04111@gmail.com>
2025-07-06 20:37:22 -0300

    estrutura de inicial
    estrutura inicial simples para codificador, ir alternado.

--- main.c
+++ main.c
@@ -1,2 +1,16 @@
 #include <stdio.h>
-#include<stdlib.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define TAM_TABELA 10
+#define TAM_CHAVE  50
+
+typedef struct No {
+    char chave[TAM_CHAVE];
+    int valor;
+    struct No* prox;
+} No;
+
+typedef struct {
+    No* lista[TAM_TABELA];
+} TabelaHash;
